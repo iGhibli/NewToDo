42b5906327a0868d1851555fba3b62151f7afbcb
iGhibli <iGhibli@163.com>
2016-02-02 14:31:18 +0800

    改变思路，新建工程

---
+++ NewToDo/NewToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewToDo
+//
+//  Created by qingyun on 16/2/2.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
