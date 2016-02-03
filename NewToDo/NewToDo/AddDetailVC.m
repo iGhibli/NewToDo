3e824ef1678f5a977cbced009a1fd68cfbca6532
iGhibli <iGhibli@163.com>
2016-02-03 19:09:31 +0800

    添加PickerView视图

--- NewToDo/NewToDo/AddDetailVC.m
+++ NewToDo/NewToDo/AddDetailVC.m
@@ -14,7 +14,8 @@
 @interface AddDetailVC ()<UIPickerViewDataSource ,UIPickerViewDelegate ,UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIPickerView *picker;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
-@property (nonatomic, strong) NSArray *dataSource;
+//@property (nonatomic, strong) NSArray *dataSource;
+@property (nonatomic, strong) NSArray *imageNames;
 @property (nonatomic, assign) NSInteger currentIndex;
 @end
 
@@ -24,7 +25,8 @@
     [super viewDidLoad];
     self.picker.delegate = self;
     self.picker.dataSource = self;
-    self.dataSource = @[@"行前事项" ,@"购物清单" ,@"文件/备份" ,@"资金" ,@"服装" ,@"个护/化妆" ,@"医疗/健康" ,@"电子数码" ,@"杂项" ,@"旅途备忘" ,@"自定义"];
+//    self.dataSource = @[@"行前事项" ,@"购物清单" ,@"文件/备份" ,@"资金" ,@"服装" ,@"个护/化妆" ,@"医疗/健康" ,@"电子数码" ,@"杂项" ,@"旅途备忘" ,@"自定义"];
+    self.imageNames = @[@"0" ,@"1" ,@"2" ,@"3" ,@"4" ,@"5" ,@"6" ,@"7" ,@"8" ,@"9" ,@"10"];
     _currentIndex = 0;
     self.textField.delegate = self;
 }
@@ -62,7 +64,6 @@
 //取消输入第一响应
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    
     [self.textField resignFirstResponder];
     return YES;
 }
@@ -75,24 +76,29 @@
 
 //每列中的行数
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
-    return self.dataSource.count;
+    return self.imageNames.count;
 }
 
 #pragma mark -UIPickerViewDelegate
-//设置文本
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    return self.dataSource[row];
+////设置文本
+//- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+//    return self.imageNames[row];
+//}
+
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:self.imageNames[row]]];
+    return imageView;
 }
 
-//设置属性文本
-- (NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    if (row == 0) {
-        NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:self.dataSource[row] attributes:@{NSUnderlineStyleAttributeName:@1,NSForegroundColorAttributeName:[UIColor redColor]}];
-        return attributedString;
-    }
-    return nil;
-    
-}
+////设置属性文本
+//- (NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component{
+//    if (row == 0) {
+//        NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:self.dataSource[row] attributes:@{NSUnderlineStyleAttributeName:@1,NSForegroundColorAttributeName:[UIColor redColor]}];
+//        return attributedString;
+//    }
+//    return nil;
+//    
+//}
 
 //设置行高
 -(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component{
